28a0e7ae5103c33298c568efe84832cc7abf82a3
sanjeev <sanjeevkmr525@gmail.com>
2020-04-28 14:44:11 +0530

    updated data type with byte and short data type

--- core-java-pgm2010/src/com/sanjeev/basic_pgm/DataType.java
+++ core-java-pgm2010/src/com/sanjeev/basic_pgm/DataType.java
@@ -1,26 +1,28 @@
 package com.sanjeev.basic_pgm;
 
 public class DataType {
-public static void main(String[] args) {
-	boolean b=true;
-	boolean f=false;
-	int a=10;
-	char c='c';
-	float fl=7.9f;
-	long lng=552221222;
-	double d=1234567891333333333l;
-    System.out.println(b);
-    System.out.println(f);
+	public static void main(String[] args) {
+		boolean b = true;
+		boolean f = false;
+		byte by = 102;
+		short s = 12344;
+		int a = 10;
+		char c = 'c';
+		float fl = 7.9f;
+		long lng = 552221222;
+		double d = 1234567891333333333l;
+		System.out.println(b);
+		System.out.println(f);
 
-    System.out.println(c);
+		System.out.println(c);
 
-    System.out.println(fl);
+		System.out.println(fl);
 
-    System.out.println(lng);
-    System.out.println(d);
+		System.out.println(lng);
+		System.out.println(d);
+	    System.out.println(by);
+	    System.out.println(s);
 
 
-    
-	
-}
+	}
 }
