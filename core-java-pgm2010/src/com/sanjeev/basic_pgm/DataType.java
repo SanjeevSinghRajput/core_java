b650e7970e03e282d34b8fa00629eb62105f0bef
sanjeev <sanjeevkmr525@gmail.com>
2020-04-28 14:38:12 +0530

    data type program committed

---
+++ core-java-pgm2010/src/com/sanjeev/basic_pgm/DataType.java
@@ -0,0 +1,26 @@
+package com.sanjeev.basic_pgm;
+
+public class DataType {
+public static void main(String[] args) {
+	boolean b=true;
+	boolean f=false;
+	int a=10;
+	char c='c';
+	float fl=7.9f;
+	long lng=552221222;
+	double d=1234567891333333333l;
+    System.out.println(b);
+    System.out.println(f);
+
+    System.out.println(c);
+
+    System.out.println(fl);
+
+    System.out.println(lng);
+    System.out.println(d);
+
+
+    
+	
+}
+}
