61dde2de6a9b8d4d48aebea3ae515047d9fd0c56
sanjeev <sanjeevkmr525@gmail.com>
2020-04-28 12:39:15 +0530

    //this is my first hello world program .

---
+++ core-java-pgm2010/src/com/sanjeev/basic_pgm/Helloworld.java
@@ -0,0 +1,10 @@
+package com.sanjeev.basic_pgm;
+//Here is the my first program Hello world printing on the console.
+public class Helloworld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World:::::");
+	}
+
+}
